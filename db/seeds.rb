f03b1e1ed6bbf8d16476375a89f10d54c13f1428
hamza.khan2 <hamza.khan@gigalabs.co>
2021-01-18 20:07:58 +0500

    Admin Views part 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-
+admin = Admin.create!(email: 'admin@test.com', password: '12345678', password_confirmation: '12345678')
 user = User.create!(email: 'john@gmail.com', password: '12345678', password_confirmation: '12345678')
 cart = Cart.create!(user_id: user.id)
 category = Category.create!(name: 'Unstitched')
